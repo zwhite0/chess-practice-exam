20c5d6e2de7c8dda1da6f8de31c96a6e127b511b
zwhite0 <156767744+zwhite0@users.noreply.github.com>
2025-05-07 23:16:42 -0600

    Initial commit

---
+++ starter-code/3-web-api/server/Server.java
@@ -0,0 +1,25 @@
+package server;
+
+import spark.*;
+
+public class Server {
+
+    public int run(int desiredPort) {
+        Spark.port(desiredPort);
+
+        Spark.staticFiles.location("web");
+
+        // Register your endpoints and handle exceptions here.
+
+        //This line initializes the server and can be removed once you have a functioning endpoint 
+        Spark.init();
+
+        Spark.awaitInitialization();
+        return Spark.port();
+    }
+
+    public void stop() {
+        Spark.stop();
+        Spark.awaitStop();
+    }
+}
